6974a1c882ea0a3403642afe42163c860cfdbd13
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-10-31 20:38:09 +0900

    add graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5, isFork: false) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface Data {
+  organization: {
+    repositories: {
+      nodes: {
+        id:string,name:string,url:string,viewerHasStarred:string,stargazers:{
+          totalCount:string
+        }
+      }[]
+    }
+  }
+}
+
+interface Variables {
+  first: number,
+  isFork: boolean
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Query<Data,Variables> query={query}>
+      {({loading, error, data})=>{
+        if (loading) return <p>lOaDiNg...</p>
+        if (!data) return <p>data not found</p>
+
+        const repositories = data.organization.repositories.nodes
+
+        return (
+          <ul>
+            {repositories.map((repo:{url:string,id:string,name:string,stargazers:{totalCount:string}})=>(
+              <li key={repo.id}>
+                <a href={repo.url}>{repo.name}</a>
+                <button>{repo.stargazers.totalCount} Star</button>
+              </li>
+            ))}
+          </ul>
+        )
+      }}
+    </Query>
   );
 }
 
